e3b039a547d729eac7d95c516273b175f293078f
Cole Pedersen <cole.pedersen@gmail.com>
2019-04-10 14:50:10 -0600

    initial commit, declarative rendering

---
+++ main.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+  // options object
+  // optional properties to store data and perform actions
+  // el plugs into the instance where the dom specifies app
+  el: "#app",
+  data: {
+    product: "Socks",
+    description: "These socks are the bees knees."
+  }
+});
+// wicked
