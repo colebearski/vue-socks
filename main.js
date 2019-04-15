86ebc93c038897db195a95165d953f546b3a3232
Cole Pedersen <cole.pedersen@gmail.com>
2019-04-15 10:32:06 -0600

    refactored to product component

--- main.js
+++ main.js
@@ -1,37 +1,87 @@
-const app = new Vue({
-  // options object
-  // optional properties to store data and perform actions
-  // el plugs into the instance where the dom specifies app
-  el: "#app",
-  data: {
-    brand: "Vue Mastery",
-    product: "Socks",
-    selectedVariant: 0,
-    description: "These socks are the bees knees.",
-    // image: "./assets/images/vmSocks-green-onWhite.jpg",
-    link: "//google.com",
-    // inStock: true,
-    // inventory: 10,
-    // onSale: true,
-    details: ["80% cotton", "20% polyester", "Gender-neutral"],
-    sizes: ["Small", "Medium", "Large"],
-    variants: [
-      {
-        variantId: 2234,
-        variantColor: "Green",
-        variantImage: "./assets/images/vmSocks-green-onWhite.jpg",
-        variantQuantity: 10,
-        onSale: true
-      },
-      {
-        variantId: 2235,
-        variantColor: "Blue",
-        variantImage: "./assets/images/vmSocks-blue-onWhite.jpg",
-        variantQuantity: 0,
-        onSale: false
-      }
-    ],
-    cart: 0
+Vue.component("product", {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true
+    }
+  },
+  template: ` <div class="product">
+  <div class="product-image">
+    <img v-bind:src="image" alt="Socks" />
+  </div>
+
+  <div class="product-info">
+   
+    <h1>{{ title }}</h1>
+    <p>{{ description }}</p>
+    <p>
+      For more information, click
+      <a v-bind:href="link" target="_blank">here</a>.
+    </p>
+    
+    <p v-if="inStock">In Stock</p>
+
+    <p class="saleItem">{{ onSale }}</p>
+
+    <p v-else :class="{ outOfStock: !inStock}">Out of Stock</p>
+    
+    <p>Shipping: {{ shipping }}</p>
+
+    <ul>
+      <li v-for="detail in details">{{ detail }}</li>
+    </ul>
+
+    <div
+      v-for="(variant, index) in variants"
+      v-bind:key="variant.variantId"
+      class="color-box"
+      v-bind:style="{backgroundColor: variant.variantColor}"
+      v-on:mouseover="updateProduct(index)"
+    >
+    </div>
+
+    <button
+      v-on:click="addToCart"
+      :disabled="!inStock"
+      :class="{ disabledButton: !inStock}"
+    >
+      Add to Cart
+    </button>
+    <button v-on:click="removeFromCart">Remove Item</button>
+
+    <div class="cart">
+      <p>Cart({{ cart }})</p>
+    </div>
+  </div>
+</div>`,
+
+  data() {
+    return {
+      brand: "Vue Mastery",
+      product: "Socks",
+      selectedVariant: 0,
+      description: "These socks are the bees knees.",
+      link: "//google.com",
+      details: ["80% cotton", "20% polyester", "Gender-neutral"],
+      sizes: ["Small", "Medium", "Large"],
+      variants: [
+        {
+          variantId: 2234,
+          variantColor: "Green",
+          variantImage: "./assets/images/vmSocks-green-onWhite.jpg",
+          variantQuantity: 10,
+          onSale: true
+        },
+        {
+          variantId: 2235,
+          variantColor: "Blue",
+          variantImage: "./assets/images/vmSocks-blue-onWhite.jpg",
+          variantQuantity: 0,
+          onSale: false
+        }
+      ],
+      cart: 0
+    };
   },
   methods: {
     addToCart: function() {
@@ -61,7 +111,20 @@ const app = new Vue({
       } else {
         return this.brand + " " + this.product + " are NOT on sale!";
       }
+    },
+    shipping() {
+      if (this.premium) {
+        return "Free";
+      } else {
+        return "$2.99";
+      }
     }
   }
 });
-// wicked
+
+const app = new Vue({
+  el: "#app",
+  data: {
+    premium: false
+  }
+});
