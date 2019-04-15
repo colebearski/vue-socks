9ea45a0c54f50445678e2813efb85c7471cc2937
Cole Pedersen <cole.pedersen@gmail.com>
2019-04-15 10:09:18 -0600

    computed properties

--- main.js
+++ main.js
@@ -4,25 +4,31 @@ const app = new Vue({
   // el plugs into the instance where the dom specifies app
   el: "#app",
   data: {
+    brand: "Vue Mastery",
     product: "Socks",
+    selectedVariant: 0,
     description: "These socks are the bees knees.",
-    image: "./assets/images/vmSocks-green-onWhite.jpg",
+    // image: "./assets/images/vmSocks-green-onWhite.jpg",
     link: "//google.com",
-    inStock: true,
-    inventory: 10,
-    onSale: true,
+    // inStock: true,
+    // inventory: 10,
+    // onSale: true,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
     sizes: ["Small", "Medium", "Large"],
     variants: [
       {
         variantId: 2234,
         variantColor: "Green",
-        variantImage: "./assets/images/vmSocks-green-onWhite.jpg"
+        variantImage: "./assets/images/vmSocks-green-onWhite.jpg",
+        variantQuantity: 10,
+        onSale: true
       },
       {
         variantId: 2235,
         variantColor: "Blue",
-        variantImage: "./assets/images/vmSocks-blue-onWhite.jpg"
+        variantImage: "./assets/images/vmSocks-blue-onWhite.jpg",
+        variantQuantity: 0,
+        onSale: false
       }
     ],
     cart: 0
@@ -34,8 +40,27 @@ const app = new Vue({
     removeFromCart: function() {
       this.cart -= 1;
     },
-    updateProduct: function(variantImage) {
-      this.image = variantImage;
+    updateProduct: function(index) {
+      this.selectedVariant = index;
+      console.log(index);
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity;
+    },
+    onSale() {
+      if (this.variants[this.selectedVariant].onSale) {
+        return this.brand + " " + this.product + " are on sale!";
+      } else {
+        return this.brand + " " + this.product + " are NOT on sale!";
+      }
     }
   }
 });
